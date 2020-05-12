4575c558a87b42d6265a553e212f256d92b42122
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-11 21:59:47 -0400

    Add model

--- demo/src/main/java/com/example/demo/model/Greeting.java
+++ demo/src/main/java/com/example/demo/model/Greeting.java
@@ -1,4 +1,23 @@
 package com.example.demo.model;
 
 public class Greeting {
+
+
+    private final long id;
+    private final String greeting;
+
+    public Greeting(long id, String greeting) {
+        this.id = id;
+        this.greeting = greeting;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+
 }
